8104338202b8159e2cdc350b8f38ce4426abd576
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2021-08-22 18:19:03 -0500

    refactor: moved Money and Portfolio classes their own Python files

---
+++ py/money.py
@@ -0,0 +1,13 @@
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+    def times(self, multiplier):
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
