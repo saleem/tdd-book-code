12ded25687d4306df238395caa15020b5bef81b8
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2021-08-22 17:44:07 -0500

    refactor: moved Money and Portfolio to stocks Go package

---
+++ go/stocks/portfolio.go
@@ -0,0 +1,16 @@
+package stocks
+
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency}
+}
