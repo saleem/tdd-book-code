41fb0dbc8309c98d31b35a86147fff4c3d50f431
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2021-08-21 22:14:02 -0500

    feat: first green test

--- js/test_money.js
+++ js/test_money.js
@@ -1,15 +1,15 @@
 const assert = require('assert');
 
 class Dollar {
-    constructor(amount) {
-      this.amount = amount;
-    }
+  constructor(amount) {
+    this.amount = amount;
+  }
 
-    times(multiplier) {
-        return new Dollar(this.amount * multiplier)
-    }
+  times(multiplier) {
+    return new Dollar(this.amount * multiplier)
+  }
 }
 
-fiver = new Dollar(5);
-tenner = fiver.times(2);
+let fiver = new Dollar(5);
+let tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 10);
--- py/test_money.py
+++ py/test_money.py
@@ -1,17 +1,20 @@
 import unittest
 
-class Dollar:
-  def __init__(self, amount):
-    self.amount = amount
 
-  def times(self, multiplier):
-    return Dollar(self.amount * multiplier)
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
+
 
 class TestMoney(unittest.TestCase):
-  def testMultiplication(self):
-    fiver = Dollar(5)
-    tenner = fiver.times(2)
-    self.assertEqual(10, tenner.amount)
+    def testMultiplication(self):
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
