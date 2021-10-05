2abb26c192c8708b65079157fac41964463f09e8
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2021-10-04 20:29:21 -0500

    chore: streamlined output of Go assertEqual function

--- go/money_test.go
+++ go/money_test.go
@@ -36,6 +36,6 @@ func TestAddition(t *testing.T) {
 
 func assertEqual(t *testing.T, expected s.Money, actual s.Money) {
 	if expected != actual {
-		t.Errorf("Expected  [%+v] Got: [%+v]", expected, actual)
+		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
